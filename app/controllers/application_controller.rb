e87c36fa292ca97d342587718e0fed5c29def1f0
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-04-29 15:14:10 +0100

    Add 7 RESTful routes with views pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,50 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to "/articles"
   end
+
+  #INDEX
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  #NEW
+  get '/articles/new' do
+    @article = Article.new
+    erb :new
+  end
+
+  #CREATE
+  post '/articles' do
+    @article = Article.create(params)
+    redirect to "/articles/#{@article.id}"
+  end
+
+  #SHOW
+  get "/articles/:id" do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  #EDIT
+  get "/articles/:id/edit" do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  #UPDATE
+  patch "/articles/:id" do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect "/articles/#{@article.id}"
+  end
+
+  delete "/articles/:id" do
+    Article.destroy(params[:id])
+    redirect to "/articles"
+  end
+
+
 end
